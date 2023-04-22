92291945441cb242f923bd8db18aba78098d0ad7
prpetrignani <prpetrignani@yhaoo.it>
2023-04-22 15:55:00 +0200

    date

--- scripts/javacode.js
+++ scripts/javacode.js
@@ -1,2 +1,6 @@
-const option = {month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
-document.getElementById('lastupdate').textContent = new Date().document.lastModified('en-US', option);
+//const option = {month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+//document.getElementById('lastupdate') = document.lastModified('en-US', option);
+let last = document.lastModified;
+document.getElementById('lastupdate').innerHTML = last;
+//var lastupdate = document.lastModified;
+//document.getElementById('lastupadate') = new lastupdate;
