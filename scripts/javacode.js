df5b5114b557de28d938ae34e3458c01948a303d
prpetrignani <prpetrignani@yhaoo.it>
2023-04-22 16:45:00 +0200

    anno

--- scripts/javacode.js
+++ scripts/javacode.js
@@ -1,5 +1,5 @@
 let last = document.lastModified;
 document.getElementById('lastupdate').innerHTML = last;
 
-const option = {year: 'numeric'};
-document.getElementById("date").textContent = new Date().toLocaleDateString('en-US', option);
+//const option = {year: 'numeric'};
+document.getElementById("date").textContent = new Date().getUTCFullYear;
