d1bb08aab57537fa5045e51df8699f09ed219cfa
prpetrignani <prpetrignani@yhaoo.it>
2023-04-22 16:05:35 +0200

    perfetto

--- scripts/javacode.js
+++ scripts/javacode.js
@@ -1,6 +1,2 @@
-//const option = {month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
-//document.getElementById('lastupdate') = document.lastModified('en-US', option);
 let last = document.lastModified;
 document.getElementById('lastupdate').innerHTML = last;
-//var lastupdate = document.lastModified;
-//document.getElementById('lastupadate') = new lastupdate;
