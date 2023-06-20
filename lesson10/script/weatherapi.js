bbe93fe7d089a8d1e1f3c0bddc2b2da04d493cdf
prpetrignani <prpetrignani@yhaoo.it>
2023-06-20 19:46:23 +0200

    img

---
+++ lesson10/script/weatherapi.js
@@ -0,0 +1,23 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid={491dc5e6a6b2f1b2d272564abc5f6dc3}&units=imperial"
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
