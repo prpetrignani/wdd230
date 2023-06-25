61570c1008666c18ec261cfbb3b0361fde7cacd4
prpetrignani <prpetrignani@yhaoo.it>
2023-06-25 23:30:24 +0200

    last

--- lesson10/chamber/scripts/windchill.js
+++ lesson10/chamber/scripts/windchill.js
@@ -1,8 +1,12 @@
-const iconaMeteo = document.querySelector('#iconaMeteo');
-const captionDesc = document.querySelector('figcaption');
 const temp = document.querySelector('#temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const wind = document.querySelector('#windSpeed')
 
 const fonte = "https://api.openweathermap.org/data/2.5/weather?q=Verona&appid=491dc5e6a6b2f1b2d272564abc5f6dc3&units=metric"
+
+
 
 async function apiFetch() {
     try {
@@ -11,7 +15,9 @@ async function apiFetch() {
         const data = await response.json();
         console.log(data); // this is for testing the call
         displayResults(data);
-      } else {
+      }
+
+      else {
           throw Error(await response.text());
       }
     } catch (error) {
@@ -21,33 +27,30 @@ async function apiFetch() {
 
 function displayResults(weatherData) {
     temp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    wind.innerHTML = `Wind Speed: ${weatherData.wind.speed.toFixed(0)} km/h`;
 
     const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
-    const desc = weatherData.weather[0].description;
   
     weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
-  
-  }
-  apiFetch();
-
-
-function myFunction (t, s) {
     
+    let t = weatherData.main.temp.toFixed(0);
+    let s = weatherData.wind.speed.toFixed(0);
+    let w = "";
+
     if (t <= 10 && s > 4.8) {
-        return "Wind Chill: " + Math.round (
+        w = "Wind Chill: " + Math.round (
             35.74 +
             0.6215 * t -
             (35.75 * s) ** 0.16 +
             (0.3965 * t) ** 0.16 
         );
+        document.getElementById("windChill").innerHTML = w.toString();
     }
 
     else {
-        return "Wind Chill: N/A";
+        w = "Wind Chill: N/A";
+        document.getElementById("windChill").innerHTML = w.toString();
     }
-}
+  }
 
-let w = myFunction(temp, wind);
-document.getElementById("chill").innerHTML = w.toString();
+apiFetch();
